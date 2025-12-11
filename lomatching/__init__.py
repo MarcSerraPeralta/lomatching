646d496ccfa4adaeb2be904dfb0350998394078b
MarcSerraPeralta <marcserraperalta@gmail.com>
2025-12-11 19:57:22 +0100

    Update to version 0.2.2

--- lomatching/__init__.py
+++ lomatching/__init__.py
@@ -2,7 +2,7 @@ from .decoder import MoMatching, BeliefMoMatching
 from .util import get_reliable_observables, remove_obs_except
 from . import util
 
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 __all__ = [
     "MoMatching",
